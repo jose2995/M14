f6c15fc63e1323a821657d7092a8263cd91068f9
jose2995 <jose.montesinos.7e6@itb.cat>
2023-11-09 22:37:26 +0100

    exercicis_basics_router fets

--- src/views/ProductDetailView.vue
+++ src/views/ProductDetailView.vue
@@ -1,3 +1,156 @@
 <template>
-    <h1>Datos del producte</h1>
+    <div>
+    <h1>Datos del producto {{ articleId }}</h1>
+    <div v-if="product">
+        <img :src="getImageUrl(product.id)" alt="Imagen del producto">
+      <h3>Nombre del producto: {{ product.name }}</h3>
+      <div>
+        <strong>Memoria:</strong> {{ product.memory.quantity }} {{ product.memory.type }}
+      </div>
+      <div>
+        <strong>Conectores:</strong> {{ product.conectors }}
+      </div>
+      <div>
+        <strong>Potencia:</strong> {{ product.power }}
+      </div>
+      <div>
+        <strong>Tamaño:</strong> Altura: {{ product.size.height }}, Profundidad: {{ product.size.depth }}
+      </div>
+    </div>
+    <div v-else>
+      <p>Producto no encontrado</p>
+    </div>
+  </div>
 </template>
+<script lang="ts" setup>
+    import { onMounted, ref, computed } from 'vue'; 
+    import {useRoute} from 'vue-router'
+
+    const productes = [
+    {
+            id: 1,
+            name: 'MSI AMD Radeon RX 6650 XT GAMING X',
+            memory: {
+                quantity: '8gb',
+                type: 'GDDR6'
+            },
+            conectors: 'DisplayPort x 3 (v1.4)',
+            power: '175 W',
+            size:{
+                height: '130mm',
+                depth: '278mm',
+            }
+        },
+        {
+            id: 2,
+            name: 'ASUS Dual GeForce RTX 4060 Ti OC Edition',
+            memory: {
+                quantity: '16gb',
+                type: 'GDDR6'
+            },
+            conectors: 'DisplayPort x 3 (v1.4a), HDMI x 1 2.1a',
+            power: '200 W',
+            size:{
+                height: '123.2mm',
+                depth: '227,2mm',
+            }
+        },
+        {
+            id: 3,
+            name: 'MSI GeForce RTX 4070 VENTUS 3X E OC',
+            memory: {
+                quantity: '12gb',
+                type: 'GDDR6'
+            },
+            conectors: 'DisplayPort x 3 (v1.4), hdmi X 1 2.1a',
+            power: '200 W',
+            size:{
+                height: '120mm',
+                depth: '308mm',
+            }
+        },
+        {
+            id: 4,
+            name: 'Sapphire NITRO+ AMD Radeon RX 7800 XT GAMING',
+            memory: {
+                quantity: '16gb',
+                type: 'GDDR6'
+            },
+            conectors: 'DisplayPort x 2 (v1.4), HDMI x 2 2.1',
+            power: '288 W',
+            size:{
+                height: '134,8mm',
+                depth: '320mm',
+            }
+        },
+        {
+            id: 5,
+            name: 'Gigabyte GeForce RTX 4090 GAMING OC',
+            memory: {
+                quantity: '24gb',
+                type: 'GDDR6X'
+            },
+            conectors: 'DisplayPort x 3 (v1.4), HDMI x 1 2.1',
+            power: ' 360W',
+            size:{
+                height: '150,2mm',
+                depth: '340mm',
+            }
+        }
+    ]
+
+    let articleId = ref();
+
+    onMounted(()=>{
+        const route = useRoute()
+        articleId.value = route.params.id
+    });
+
+    const product = computed(() => {
+        return productes[articleId.value - 1];
+    });
+
+    const getImageUrl = (articleId:number) => {
+        return `/imagenes/tarjeta${articleId}.jpg`;
+    };    
+</script>
+
+<style scoped>
+    .product-details {
+    max-width: 600px;
+    margin: 0 auto;
+    padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 8px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  }
+
+  h1 {
+    text-align: center;
+    color: #333;
+  }
+
+  h3 {
+    margin-top: 10px;
+  }
+
+  div {
+    margin-bottom: 10px;
+  }
+
+  strong {
+    color: #007BFF;
+  }
+
+  img {
+    width: 500px;
+    height: auto;
+    border-radius: 8px;
+    margin-top: 10px;
+  }
+
+  .not-found {
+    text-align: center;
+    color: #FF0000;
+  }
+</style>
--- src/views/ProductListView.vue
+++ src/views/ProductListView.vue
@@ -1,13 +1,14 @@
 <template>
-    <h1>Aquí va el llistat de productes</h1>
+    <div class="product-list">
+    <h1>Aquí va el listado de productos</h1>
     <ul>
-        <li v-for="producto in productes" :key="producto.name">
-            <router-link :to="{name: 'product',params:{id:producto.id}}" :name="producto.name" 
-            :memoria="producto.memory.quantity" :memoriaTipus="producto.memory.type"
-            :conectors="producto.conectors" :power="producto.power" 
-            :height="producto.size.height" :depth="producto.size.depth">Ver detalles del producto {{ producto.name }}</router-link>
-        </li>
+      <li v-for="producto in productes" :key="producto.id" class="product-item">
+        <router-link :to="{ name: 'product', params: { id: producto.id } }" class="product-link">
+          Ver detalles del producto {{ producto.name }}
+        </router-link>
+      </li>
     </ul>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -15,72 +16,58 @@
         {
             id: 1,
             name: 'MSI AMD Radeon RX 6650 XT GAMING X',
-            memory: {
-                quantity: '8gb',
-                type: 'GDDR6'
-            },
-            conectors: 'DisplayPort x 3 (v1.4)',
-            power: '175 W',
-            size:{
-                height: '130mm',
-                depth: '278mm',
-            }
         },
         {
             id: 2,
             name: 'ASUS Dual GeForce RTX 4060 Ti OC Edition',
-            memory: {
-                quantity: '16gb',
-                type: 'GDDR6'
-            },
-            conectors: 'DisplayPort x 3 (v1.4a), HDMI x 1 2.1a',
-            power: '200 W',
-            size:{
-                height: '123.2mm',
-                depth: '227,2mm',
-            }
         },
         {
             id: 3,
             name: 'MSI GeForce RTX 4070 VENTUS 3X E OC',
-            memory: {
-                quantity: '12gb',
-                type: 'GDDR6'
-            },
-            conectors: 'DisplayPort x 3 (v1.4), hdmi X 1 2.1a',
-            power: '200 W',
-            size:{
-                height: '120mm',
-                depth: '308mm',
-            }
         },
         {
             id: 4,
             name: 'Sapphire NITRO+ AMD Radeon RX 7800 XT GAMING',
-            memory: {
-                quantity: '16gb',
-                type: 'GDDR6'
-            },
-            conectors: 'DisplayPort x 2 (v1.4), HDMI x 2 2.1',
-            power: '288 W',
-            size:{
-                height: '134,8mm',
-                depth: '320mm',
-            }
         },
         {
             id: 5,
             name: 'Gigabyte GeForce RTX 4090 GAMING OC',
-            memory: {
-                quantity: '24gb',
-                type: 'GDDR6X'
-            },
-            conectors: 'DisplayPort x 3 (v1.4), HDMI x 1 2.1',
-            power: ' 360W',
-            size:{
-                height: '150,2mm',
-                depth: '340mm',
-            }
         }
     ]
 </script>
+
+<style scoped>
+.product-list {
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+h1 {
+  text-align: center;
+  color: #333;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+.product-item {
+  margin: 10px 0;
+  padding: 10px;
+  background-color: #f9f9f9;
+  border-radius: 8px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+}
+
+.product-link {
+  text-decoration: none;
+  color: #007BFF;
+  font-weight: bold;
+  transition: color 0.3s ease;
+}
+
+.product-link:hover {
+  color: #0056b3;
+}
+</style>
